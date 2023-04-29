b2233dd81391d094b947984c207ed0056bccb6bf
Yolian Boister <yolianboister@gmail.com>
2023-04-29 22:17:41 +0100

    Added subtraction

--- Help/calcuator.py
+++ Help/calcuator.py
@@ -3,20 +3,42 @@ import tkinter
 import time
 
 
-features = "Current features include: eggs and bread."
-
+features = "Current features include: Combining two numbers."
 
 def intro():
     print(features, "Have fun!")
 
+def func_subtract():
+    num1 = input("First number? ")
+    num2 = input("Second number? ")
+    num1 = int(num1)
+    num2 = int(num2)
+    global finalnum
+    finalnum = num1 - num2
+
+def func_add():
+    num1 = input("First number? ")
+    num2 = input("Second number? ")
+    num1 = int(num1)
+    num2 = int(num2)
+    global finalnum
+    finalnum = num1 + num2
+
+
 print("Welcome to calcuator by Yolian.")
 intro()
 
+choice1 = input("Would you like to subtract or add?")
 
-num1 = input("First number?")
-num1 = int(num1)
-num2 = input("Second number?")
-num2 = int(num2)
+if choice1 == "subtract":
+    func_subtract()
+elif choice1 == "Subtract":
+    func_subtract()
+elif choice1 == "add":
+    func_add()
+elif choice1 == "Add":
+    func_add()
+else: print("I don't undertsand that")
 
-finalnum = num1 + num2
-print ("Your number is:",finalnum)
+global finalnum
+print ("Your final number is:",finalnum)
